0f2df92b1bdf2426033b779ff3ecf8fc0d6d163f
Loic Poulain <loic.poulain@gmail.com>
2016-07-06 11:32:52 +0200

    Add types header

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,44 @@
+#include "types.h"
 
-extern void print_hello(void);
+void init_kernel();
 
-void start_kernel()
-{
-	print_hello();
+void start_kernel() {
+	init_kernel();
 	while(1);
+}
+
+void terminal_print_string(const char *str, unsigned int x, unsigned int y)
+{
+	/* Todo: fix this static computation */
+	volatile u8* vram = (volatile u8*) 0xB8000 + 2 * (x + y * 80);
+
+	while (*str) {
+		*vram++ = *str++;
+		*vram++ = 0x57;
+	}
+}
+
+u32 rdtsc(void) {
+	  u32 x;
+	  __asm__ volatile ("rdtsc" : "=A" (x));
+	  return x;
+}
+
+void *memcpy(void *dst, const void *src, int size)
+{
+	void *p = dst;
+
+	while (size--)
+		*(u8 *)dst++ = *(u8 *)src++;
+
+	return p;
+}
+
+void init_kernel()
+{
+	char* terminal_buffer = (char*) 0xB8000;
+	char* terminal_color = (char*) 0xB8001;
+	char hello[] = "HELLO WORLD";
+
+	terminal_print_string(hello, 0, 1);
 }
